d31a7b72149140c7d27b3fde296d90e546cc1956
copecustomkicks <aidan.copeland.21@gmail.com>
2026-08-24 21:11:34 -0400

    Clean up LinkedIn profile link code
    Removed redundant LinkedIn profile link code.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -95,13 +95,6 @@ export default function AboutPage() {
                         LinkedIn profile
                       </a>
                     ) : null}
-                        className="text-uf-blue hover:underline"
-                        rel="noreferrer noopener"
-                        target="_blank"
-                      >
-                        LinkedIn profile
-                      </a>
-                    ) : null}
                   </li>
                   <li className="border-t border-rule pt-3">
                     {site.resume ? (
